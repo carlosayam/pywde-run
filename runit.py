74a81193082166e1fcbc94c283782949af5bd833
Carlos Aya <carlosayam@users.noreply.github.com>
2019-02-04 22:51:46 +1100

    wde batcher

--- runit.py
+++ runit.py
@@ -1,6 +1,10 @@
 import numpy as np
 import math
 import random
+import pathlib
+import os
+from datetime import datetime
+from collections import namedtuple
 #import matplotlib.pyplot as plt
 import itertools as itt
 import csv
@@ -588,6 +592,8 @@ def plot_kde(kde, fname, dist, zlim):
 
 
 def hellinger_distance_wip(dist, dist_est):
+    # import code
+    # code.interact(local=locals())
     def ferr(x, y):
         args = np.array([(x, y)])
         pdf_vals = np.sqrt(dist.pdf(args))
@@ -624,12 +630,97 @@ def hellinger_distance(dist, dist_est):
     return err, corr_factor
 
 
+ROOT_DIR = pathlib.Path('RUN01')
+NUM_SAMPLES = 50
+
 def fname(what, dist_name, num=None, wave_name=None, delta_j=None, ext='.png'):
     strn = '%04d' % num if num is not None else None
     strd = '%d' % delta_j if delta_j is not None else None
     strs = [dist_name, what, strn, wave_name, strd]
     strs = [v for v in strs if v]
-    return 'pngs/%s%s' % ('-'.join(strs), ext)
+    return '%s/%s%s' % (ROOT_DIR, '-'.join(strs), ext)
+
+
+def ensure_dir(a_path):
+    if not a_path.exists():
+        os.makedirs(a_path.absolute())
+    return a_path
+
+
+def base_dir(dist_name, num_obvs):
+    return ensure_dir(ROOT_DIR / dist_name / ('%04d' % num_obvs))
+
+
+def sample_name(dist_name, num_obvs, sample_no):
+    filename = 'sample-%02d.tab' % sample_no
+    path = ensure_dir(base_dir(dist_name, num_obvs) / 'samples') / filename
+    return path.absolute()
+
+
+def results_name(dist_name, num_obvs, sample_no, what):
+    filename = '%s-%02d.tab' % (what, sample_no)
+    path = ensure_dir(base_dir(dist_name, num_obvs) / 'results') / filename
+    return path.absolute()
+
+
+Result = namedtuple('Result', [
+    'dist_code',
+    'num_obvs',
+    'sample_no',
+    'what', # KDE, WDE
+    'wave_name',
+    'k',
+    'delta_j',
+    'loss',
+    'ordering',
+    'hd',
+    'params',
+    'elapsed_time'
+])
+Result.__new__.__defaults__ = ('', 0, 0, '', 0, 0, '', '', 0.0, 0, 0.0)
+
+
+def result_kde(dist_code, num_obvs, sample_no, hd, elapsed):
+    return Result(
+        dist_code=dist_code,
+        num_obvs=num_obvs,
+        sample_no=sample_no,
+        what='KDE',
+        hd=hd,
+        elapsed_time=elapsed
+    )
+
+
+def result_wde_classic(dist_code, num_obvs, sample_no, wave_name, k, delta_j, params, hd, elapsed):
+    return Result(
+        dist_code=dist_code,
+        num_obvs=num_obvs,
+        sample_no=sample_no,
+        what='WDE_FULL',
+        wave_name=wave_name,
+        k=k,
+        delta_j=delta_j,
+        params=params,
+        hd=hd,
+        elapsed_time=elapsed
+    )
+
+def result_wde_cv(dist_code, num_obvs, sample_no, wave_name, k, delta_j, params, loss, ordering, hd, elapsed):
+    return Result(
+        dist_code=dist_code,
+        num_obvs=num_obvs,
+        sample_no=sample_no,
+        what='WDE_CV',
+        wave_name=wave_name,
+        k=k,
+        delta_j=delta_j,
+        params=params,
+        loss=loss,
+        ordering=ordering,
+        hd=hd,
+        elapsed_time=elapsed
+    )
+
 
 
 def save_data(data, fname):
@@ -638,15 +729,146 @@ def save_data(data, fname):
         for row in data:
             writer.writerow(row)
 
+
+def read_data(fname):
+    with open(fname, 'rt') as fh:
+        reader = csv.reader(fh, delimiter='\t', quoting=csv.QUOTE_NONNUMERIC)
+        data = []
+        for row in reader:
+            data.append(row)
+        return np.array(data)
+
+
+
 @click.group()
 def main():
     pass
+
 
 @main.command()
 @click.argument('dist_name', metavar="DIST_CODE")
 def plot_true(dist_name):
     dist = dist_from_code(dist_name)
     plot_dist(fname('true', dist_name), dist)
+
+
+@main.command()
+@click.argument('dist_code')
+@click.argument('num_obvs', type=int)
+def gen_samples(dist_code, num_obvs):
+    """
+    Generates 50 samples for given distribution and number of observations
+    in each sample.
+    """
+    dist = dist_from_code(dist_code)
+    for ix in range(NUM_SAMPLES):
+        dest = sample_name(dist_code, num_obvs, ix+1)
+        data = dist.rvs(num_obvs)
+        save_data(data, dest)
+
+
+@main.command()
+@click.argument('dist_code')
+@click.argument('num_obvs', type=int)
+@click.argument('sample_no', type=int, default='0')
+def calc_kde(dist_code, num_obvs, sample_no):
+    """
+    Calculates KDE
+    :param dist_code:
+    :param num_obvs:
+    :param sample_no:
+    :return:
+    """
+    def gen():
+        for ix in sample_range:
+            source = sample_name(dist_code, num_obvs, ix+1)
+            data = read_data(source)
+            assert data.shape[0] == num_obvs
+            t0 = datetime.now()
+            kde = KDEMultivariate(data, 'c' * data.shape[1], bw='cv_ml')  ## cv_ml
+            elapsed = (datetime.now() - t0).total_seconds()
+            hd, corr_factor = hellinger_distance_wip(dist, kde)
+            yield result_kde(dist_code, num_obvs, sample_no, hd, elapsed)
+
+    dist = dist_from_code(dist_code)
+    if sample_no == 0:
+        sample_range = range(NUM_SAMPLES)
+        result_file = results_name(dist_code, num_obvs, 0, 'kde')
+    else:
+        sample_range = [sample_no]
+        result_file = results_name(dist_code, num_obvs, sample_no, 'kde')
+    with open(result_file, 'wt') as fh:
+        writer = csv.writer(fh, delimiter='\t')
+        for result in gen():
+            writer.writerow(list(result))
+
+
+@main.command()
+@click.argument('dist_code')
+@click.argument('num_obvs', type=int)
+@click.argument('sample_no', type=int, default='0')
+@click.argument('wave_name')
+@click.option('--k', type=int)
+@click.option('--j0', type=int)
+@click.option('--delta-j', type=int)
+def calc_wde(dist_code, num_obvs, sample_no, wave_name, **kwargs):
+    """
+    Calculates WDE for given k, j0 and delta-j for all possible options
+    """
+    def gen():
+        for ix, k, j0, delta_j in itt.product(sample_range, k_range, j0_range, delta_j_range):
+            source = sample_name(dist_code, num_obvs, ix+1)
+            data = read_data(source)
+            assert data.shape[0] == num_obvs
+            t0 = datetime.now()
+            wde = WaveletDensityEstimator(((wave_name, 0),(wave_name, 0)) , k=k, delta_j=delta_j)
+            wde.fit(data)
+            elapsed = (datetime.now() - t0).total_seconds()
+            hd, corr_factor = hellinger_distance(dist, wde)
+            params = wde.pdf.nparams
+            yield result_wde_classic(dist_code, num_obvs, sample_no, wave_name, k, delta_j, params, hd, elapsed)
+            for loss, ordering in WaveletDensityEstimator.valid_options():
+                t0 = datetime.now()
+                wde.cvfit(data, loss, ordering)
+                elapsed = (datetime.now() - t0).total_seconds()
+                hd, corr_factor = hellinger_distance(dist, wde)
+                params = wde.pdf.nparams
+                yield result_wde_cv(dist_code, num_obvs, sample_no, wave_name, k, delta_j, params, loss, ordering, hd, elapsed)
+
+
+
+    dist = dist_from_code(dist_code)
+    what = 'wde'
+    if 'k' not in kwargs:
+        k_range = [1, 2]
+    else:
+        k = kwargs['k']
+        k_range = [k]
+        what = what + ('.k_%d' % k)
+    if 'j0' not in kwargs:
+        j0_range = [0]
+    else:
+        j0 = kwargs['j0']
+        j0_range = [j0]
+        what = what + ('.j0_%d' % j0)
+    if 'delta_j' not in kwargs:
+        delta_j_range = range(0, 6)
+    else:
+        delta_j = kwargs['delta_j']
+        delta_j_range = [delta_j]
+        what = what + ('.delta_j_%d' % delta_j)
+    what = wave_name + '-' + what
+    if sample_no == 0:
+        sample_range = range(NUM_SAMPLES)
+        result_file = results_name(dist_code, num_obvs, 0, what)
+    else:
+        sample_range = [sample_no]
+        result_file = results_name(dist_code, num_obvs, sample_no, what)
+    with open(result_file, 'wt') as fh:
+        writer = csv.writer(fh, delimiter='\t')
+        for result in gen():
+            writer.writerow(list(result))
+
 
 
 @main.command()
@@ -674,8 +896,7 @@ def calc_with(dist_name, wave_name, num, i0, numi, delta_js):
     for ix in range(numi):
         data = dist.rvs(num)
         i = i0 + ix
-        save_data(data, fname('data', dist_name, num=num, wave_name=wave_name, ext='(%02d).csv' % i))
-        print('===== %02d =====' % i)
+        # save_data(data, fname('data', dist_name, num=num, wave_name=wave_name, ext='(%02d).csv' % i))
         for k, delta_j in itt.product([1,2], delta_js):
             wde = WaveletDensityEstimator(((wave_name, 0),(wave_name, 0)) , k=k, delta_j=delta_j)
             print('WDE', 'k=%d' % k, 'delta_j=%d' % delta_j)
@@ -693,9 +914,9 @@ def calc_with(dist_name, wave_name, num, i0, numi, delta_js):
                 # what = 'energy_%s.%s' % (loss, ordering)
                 # plot_energy(wde, fname(what, dist_name, num, wave_name, delta_j))
         #print('Estimating KDE all data')
-        kde = KDEMultivariate(data, 'c' * data.shape[1], bw='cv_ml') ## cv_ml
-        hd, corr_factor = hellinger_distance(dist, kde)
-        yield [dist_name, wave_name, num, i, 'KDE', '', '', '', '', hd]
+        # kde = KDEMultivariate(data, 'c' * data.shape[1], bw='cv_ml') ## cv_ml
+        # hd, corr_factor = hellinger_distance(dist, kde)
+        # yield [dist_name, wave_name, num, i, 'KDE', '', '', '', '', hd]
         # plot_kde(kde, fname('kde_cv', dist_name, num), dist, 1.1 * max_v)
 
 
